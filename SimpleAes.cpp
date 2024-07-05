206c2f728c76f39dfbfd8a28102e5edcbc4376b4
root <root@DESKTOP-TA10UGO>
2024-07-04 17:17:12 -0700

    [ADD] aes complete

--- SimpleAes.cpp
+++ SimpleAes.cpp
@@ -6,10 +6,11 @@
 *
 *
 */
-
+#include <bitset>
 #include "SymmetricEncryption.hpp"
 #include "types.hpp"
 #include <iostream>
+#include <string>
 
 int8 SimpleAes::subNib(int8 b) {
 	int8 n0 = b & 0x0f;
@@ -53,27 +54,89 @@ struct int48 SimpleAes::expandKey(){
 
 	struct int48 key = {w0, w1, w2, w3, w4, w5};
 
-	std::cout << "Original short number: " << std::hex << _key << std::endl;	    
-	std::cout << "W0 " << std::hex << static_cast<int>(w0) << std::endl;
-	std::cout << "W1 " << std::hex << static_cast<int>(w1) << std::endl;
-	std::cout << "W2 " << std::hex << static_cast<int>(w2) << std::endl;
-	std::cout << "W3 " << std::hex << static_cast<int>(w3) << std::endl;
-	std::cout << "W4 " << std::hex << static_cast<int>(w4) << std::endl;
-	std::cout << "W5 " << std::hex << static_cast<int>(w5) << std::endl;
+//	std::cout << std::bitset<8>(key.w0) << std::endl;
+//	std::cout << std::bitset<8>(key.w1) << std::endl;
+//	std::cout << std::bitset<8>(key.w2) << std::endl;
+//	std::cout << std::bitset<8>(key.w3) << std::endl;
+//	std::cout << std::bitset<8>(key.w4) << std::endl;
+//	std::cout << std::bitset<8>(key.w5) << std::endl;
+	
 	return key;
+}
+void shiftRow(int8&b1, int8&b2) {
+	int8 n1 = b1 & 0x0f;
+	int8 n2 = b1 >> 4;
+	int8 n3 = b2 & 0x0f;
+	int8 n4 = b2 >> 4;
+	b1 = (n2 << 4) | (n3 << 0);
+	b2 = (n4 << 4) | (n1 << 0);
+}
+int8 mixCol(int8 b) {
 
+	int8 c;
+	int8 seven = b >> 0 & 1;
+	int8 six = b >> 1 & 1;
+	int8 five = b >> 2 & 1;
+	int8 four = b >> 3 & 1;
+	int8 three = b >> 4 & 1;
+	int8 two = b  >> 5 & 1;
+	int8 one = b >> 6 & 1;
+	int8 zero = b >> 7 & 1;
+
+	c = 
+		(six ^ zero) << 7 |
+		(four ^ one ^ seven) << 6 |
+		(four ^ two ^ five) << 5 |
+		(three ^ five) << 4 |
+		(two ^ four) << 3 |
+		(zero ^ five ^ three) << 2 |
+		(one ^ zero ^ six) << 1 |
+		(seven ^ one) << 0;
+		
+
+	return c;
 }
 void SimpleAes::encrypt(std::string message) {
 	if(message.size()!=2) return;	
 	struct int48 key = expandKey();
-	
-
-
-
-
-
 
 	
+	int8 temp0 = int8((char)message[0]);
+	int8 temp1 = int8((char)message[1]);
+
+	std::cout <<"Plain Text " << std::bitset<8>(temp0) << std::endl << std::bitset<8>(temp1) << std::endl;
+	temp0 = temp0 ^ key.w0;
+	temp1 = temp1 ^ key.w1;
+
+	std::cout << "Add Key" << std::bitset<8>(temp0) << std::endl << std::bitset<8>(temp1) << std::endl;
+	temp0 = subNib(temp0);
+	temp1 = subNib(temp1);
+
+	shiftRow(temp0, temp1);
+
+	temp0 = mixCol(temp0);
+	temp1 = mixCol(temp1);
+	
+	std::cout <<"Sub Shit Mix Key" << std::bitset<8>(temp0) << std::endl << std::bitset<8>(temp1) << std::endl;
+	temp0 = temp0 ^ key.w2;
+	temp1 = temp1 ^ key.w3;
+
+	//std::cout << std::bitset<8>(temp0) << std::endl << std::bitset<8>(temp1) << std::endl;
+	temp0 = subNib(temp0);
+	temp1 = subNib(temp1);
+
+	shiftRow(temp0, temp1);
+	
+	temp0 = temp0 ^ key.w4;
+	temp1 = temp1 ^ key.w5;
+
+	std::cout << std::bitset<8>(temp0) << std::endl << std::bitset<8>(temp1) << std::endl;
+	std::string res = "";
+	res += char(temp0);
+	res += char(temp1);
+
+	std::cout <<"Cipher Text: "<< res << std::endl;
+
 }
 void SimpleAes::decrypt() {
 	
--- aes_test.cpp
+++ aes_test.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int main(){
 	cout << "AES Tests" << endl;
 	SimpleAes aes;
-	aes.setKey(0x3BA7);
-	aes.encrypt("This is the message");
+	aes.setKey(0x3ba7);
+	aes.encrypt("ok");
 
 	return 0;
 }
