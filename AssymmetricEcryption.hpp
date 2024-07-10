55dd3a94ea7d433dcdf627f04748239d223711f5
8coolguy <arnavchoudhury@mittymonarch.com>
2024-07-10 00:10:29 -0700

    [ADD] header file

--- AssymmetricEcryption.hpp
+++ AssymmetricEcryption.hpp
@@ -7,6 +7,15 @@ class AssymetricEcryption
 {
 public:
 	AssymetricEcryption();
+	virtual void generateKeys();
+	virtual std::string encrypt(int8 key,std::string message);
+	virtual std::string decrypt(std::string cipherText);
+};
+
+class Rsa
+{
+public:
+	Rsa();
 };
 
 #endif 
