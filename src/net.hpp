0ac43c254d1e9f166d6e9697f5b29cac61cf8cd6
8coolguy <arnavchoudhury@mittymonarch.com>
2024-08-24 01:08:08 -0700

    [ADD] server pages

---
+++ src/net.hpp
@@ -0,0 +1,24 @@
+#pragma once //https://stackoverflow.com/questions/5776910/what-does-pragma-once-mean-in-c
+#ifndef NET_H
+#define NET_H
+#include "mongoose.h"
+
+#if defined(HTTP_URL) && defined(HTTPS_URL)
+#elif MG_ENABLE_TCPIP
+#define HTTP_URL "http://0.0.0.0:80"     // Embedded build:
+#define HTTPS_URL "https://0.0.0.0:443"  // Use standard privileged ports
+#else
+#define HTTP_URL "http://0.0.0.0:8000"    // Workstation build:
+#define HTTPS_URL "https://0.0.0.0:8443"  // Use non-privileged ports
+#endif
+
+#define FS (&mg_fs_posix) 
+#define ROOT_DIR "./web_root"
+
+void net_init(struct mg_mgr *mgr);
+
+
+#endif // !NET
+
+
+
--- src/server.cpp
+++ src/server.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "net.hpp"
 #include "mongoose.h"   // To build, run: cc main.c mongoose.c
-// HTTP server event handler function
-void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
-	if (ev == MG_EV_HTTP_MSG) {
-		struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-		struct mg_http_serve_opts opts = { .root_dir = "./" };
-		mg_http_serve_dir(c, hm, &opts);
-	}
-}
-void login_handler(struct mg_connection *c, int ev, void *ev_data) {
-	if (ev == MG_EV_HTTP_MSG) {
-		struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-		struct mg_http_serve_opts opts = { .root_dir = "./" };
-		mg_http_serve_dir(c, hm, &opts);
-	}
-}
 
-int main(void) {
-	  struct mg_mgr mgr;  // Declare event manager
-	  mg_mgr_init(&mgr);  // Initialise event manager
-	  mg_http_listen(&mgr, "http://0.0.0.0:8000", ev_handler, NULL);  // Setup listener
-	  for (;;) {          // Run an infinite event loop
+// HTTP server event handler function
+static void run_mongoose(void) {
+	struct mg_mgr mgr;
+	mg_mgr_init(&mgr);
+	mg_log_set(MG_LL_DEBUG);
+	net_init(&mgr);
+	for (;;) {
 		mg_mgr_poll(&mgr, 1000);
-	  }
-	  return 0;
+	}
+}
+int main(void) {
+	run_mongoose();	
+	return 0;
 }
